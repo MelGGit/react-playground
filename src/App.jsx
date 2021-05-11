602891eb327a6f942d43d0a0c594a534b8d40864
Mel Grosser <grosser.mel@live.de>
2021-05-11 16:08:52 +0200

    New style for Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import * as React from 'react'
 import Card from './Card'
 import Header from './Header'
 import Pagination from './Pagination'
+import trivia from './api.php.json'
+import './App.css'
 
 export default () => {
   return (
-    <div>
-      <Pagination totalPages={34} currentPage={1} />
-      <Pagination totalPages={34} currentPage={2} />
-      <Pagination totalPages={34} currentPage={34} />
+    <div className="App">
+      {trivia.map(el => {
+        const { category, question, correct_answer, incorrect_answers } = el
+        return (
+          <Card
+            key={correct_answer}
+            category={category}
+            question={question}
+            correctAnswer={correct_answer}
+            tagList={incorrect_answers}
+          />
+        )
+      })}
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,35 @@
 import * as React from 'react'
 import './Card.css'
 
-export default ({ title, checked }) => {
+export default function Card({
+  category,
+  question,
+  correctAnswer,
+  tagList,
+  showAnswer,
+  checked,
+}) {
   return (
     <section className="Card">
-      <h2>{title}</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates,
-        in.
+      <h2 className="Card__type">{category}</h2>
+      <p className="Card__question">{question}</p>
+      <p
+        className={
+          showAnswer ? 'Card__answer' : 'Card__answer Card__hideAnswer'
+        }
+      >
+        {correctAnswer}
       </p>
-      <div className={checked ? 'bookmark checked' : 'bookmark'}></div>
+      <ul className="Card__tagList">
+        {tagList.map(tag => {
+          return (
+            <li key={tag} className="Card__tag">
+              {tag}
+            </li>
+          )
+        })}
+      </ul>
+      <button className={checked ? 'bookmark checked' : 'bookmark'}></button>
     </section>
   )
 }
